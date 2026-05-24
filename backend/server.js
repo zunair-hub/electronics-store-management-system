2bfd1c97b7ca81bb856cb9702318d5017cfae37d
Zunair Zafar <zed.digitally@gmail.com>
2026-05-24 20:51:05 +1000

    Corrected PORT in backend server

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res.send("Electronics Store Management Backend Running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
