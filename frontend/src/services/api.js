0267ec4e5f0a0ee2ef24a339704f1698436559bb
Zunair Zafar <zed.digitally@gmail.com>
2026-05-24 20:45:33 +1000

    API / PORT Address corrected

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://3.106.54.17/:5001/api",
+  baseURL: "http://3.106.54.17:5001/api",
 });
 
 API.interceptors.request.use((req) => {
