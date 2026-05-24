30adabc7a2f46c7bf191ad36c176fbc5d3231061
Zunair Zafar <zed.digitally@gmail.com>
2026-05-24 11:25:03 +1000

    Implement admin and user dashboards

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,10 +1,35 @@
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
 function Dashboard() {
+  const role = localStorage.getItem("role");
+  const name = localStorage.getItem("name");
+
   return (
     <div>
       <Header />
-      <h2>Dashboard Page</h2>
+
+      <div className="dashboard-container">
+        <h2>Welcome {name}</h2>
+
+        {role === "admin" ? (
+          <div className="dashboard-card">
+            <h3>Admin Dashboard</h3>
+            <p>You can manage electronic products in the system.</p>
+            <Link to="/products">
+              <button>Manage Products</button>
+            </Link>
+          </div>
+        ) : (
+          <div className="dashboard-card">
+            <h3>User Dashboard</h3>
+            <p>You can view and search available electronic products.</p>
+            <Link to="/products">
+              <button>View Products</button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
