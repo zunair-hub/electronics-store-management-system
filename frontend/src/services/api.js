16933360f92b531b61e3c98b99e15ed9624c3606
Zunair Zafar <zed.digitally@gmail.com>
2026-05-29 10:18:43 +1000

    Update deployment public IP

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://54.153.238.95:5001/api",
+  baseURL: "http://13.236.117.26:5001/api",
 });
 
 API.interceptors.request.use((req) => {
